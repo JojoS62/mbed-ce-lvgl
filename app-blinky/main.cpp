ae29228807086833d0127fb817fba2e65d549d42
JojoS <sn2010@onlinehome.de>
2023-01-20 09:32:04 +0100

    add custom target ROBOTDYN_F303CC

--- app-blinky/main.cpp
+++ app-blinky/main.cpp
@@ -1,8 +1,18 @@
 #include "mbed.h"
 
+DigitalOut led(LED1);
+DigitalOut testPin(PA_4);
+
 int main()
 {
-	while(true) {}
+	printf("hello world from \n");
+	
+	while(true) {
+		led = !led;
+		testPin = !testPin;
+
+		ThisThread::sleep_for(100ms);
+	}
 
 	// main() is expected to loop forever.
 	// If main() actually returns the processor will halt
